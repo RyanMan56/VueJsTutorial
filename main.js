94688faf09a715c0571edd5dbced690862693cc9
Ryan Jones <RyanMan56@gmail.com>
2020-07-19 22:53:22 +0100

    Completed lesson 7

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
-    inStock: true,
+    selectedVariant: 0,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
         variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
         variantColor: 'blue',
         variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0,
       }
     ],
     cart: 0,
@@ -23,8 +25,20 @@ var app = new Vue({
     addToCart: function() {
       this.cart += 1;
     },
-    updateProduct: function(variantImage) {
-      this.image = variantImage;
+    updateProduct: function(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    }
+  },
+  computed: {
+    title: function() {
+      return this.brand + ' ' + this.product;
+    },
+    image: function() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock: function() {
+      return this.variants[this.selectedVariant].variantQuantity;
     }
   }
 });
