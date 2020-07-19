8820156b5eb087fc4534a84db0f6f702dcb5007d
Ryan Jones <RyanMan56@gmail.com>
2020-07-19 23:23:01 +0100

    Completed lesson 9

--- main.js
+++ main.js
@@ -34,11 +34,7 @@ Vue.component('product', {
           v-on:click="addToCart"
           :disabled="!inStock"
           :class="{ disabledButton: !inStock, strikethrough: !inStock }"
-        >Add to Cart</button>
-
-        <div class="cart">
-          <p>Cart({{cart}})</p>
-        </div>
+        >Add to Cart</button>        
       </div>
     </div>
   `,
@@ -62,16 +58,14 @@ Vue.component('product', {
           variantQuantity: 0,
         }
       ],
-      cart: 0,
     }
   },
   methods: {
     addToCart: function() {
-      this.cart += 1;
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
     },
     updateProduct: function(index) {
       this.selectedVariant = index;
-      console.log(index);
     }
   },
   computed: {
@@ -97,5 +91,11 @@ var app = new Vue({
   el: '#app',
   data: {
     premium: false,
+    cart: [],
+  },
+  methods: {
+    updateCart: function(id) {
+      this.cart.push(id);
+    }
   }
 });
