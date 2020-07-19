1eee6ccd859c901397cbc3692959da315de1762e
Ryan Jones <RyanMan56@gmail.com>
2020-07-19 22:23:12 +0100

    Completed lesson 5

--- main.js
+++ main.js
@@ -9,12 +9,25 @@ var app = new Vue({
       {
         variantId: 2234,
         variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
       },
       {
         variantId: 2235,
         variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
       }
     ],
-    sizes: ['small', 'medium', 'large'],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    removeFromCart: function() {
+      this.cart -= 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
