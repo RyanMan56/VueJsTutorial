9df559f9fa5310c0f75c8299c2955f743d4ca1d0
Ryan Jones <RyanMan56@gmail.com>
2020-07-19 15:03:48 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'Rlly comfy, 10/10',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'https://www.google.com',
+    inventory: 0,
+  }
+});
