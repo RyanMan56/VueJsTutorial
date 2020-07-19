4fc4ce898e444d0ee620f5229c8e5b4e16356dcc
Ryan Jones <RyanMan56@gmail.com>
2020-07-19 15:14:17 +0100

    Completed lesson 4

--- main.js
+++ main.js
@@ -2,10 +2,19 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'Rlly comfy, 10/10',
     image: './assets/vmSocks-green-onWhite.jpg',
-    link: 'https://www.google.com',
-    inventory: 0,
-    onSale: true,
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+      }
+    ],
+    sizes: ['small', 'medium', 'large'],
   }
 });
